c2f85e78b59e00f0b7d737ea9efaeb403d3c4de8
Lothar Wendehals <lothar.wendehals@itemis.de>
2016-07-12 13:25:03 +0200

    Copyright headers added.

--- plugins/org.yakindu.sct.arduino.generator.cpp/src/org/yakindu/sct/arduino/generator/cpp/features/Messages.java
+++ plugins/org.yakindu.sct.arduino.generator.cpp/src/org/yakindu/sct/arduino/generator/cpp/features/Messages.java
@@ -1,13 +1,24 @@
+/**
+ * Copyright (c) 2016 by Lothar Wendehals.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.yakindu.sct.arduino.generator.cpp.features;
 
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
+
 	private static final String BUNDLE_NAME = "org.yakindu.sct.arduino.generator.cpp.features.messages"; //$NON-NLS-1$
+
 	public static String Timer_ATmega168328_description;
 	public static String Timer_ATmega168328_title;
 	public static String Timer_software_description;
 	public static String Timer_software_title;
+
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
